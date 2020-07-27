1c9cf992d8b08dd016ed071b85b8c3ba225df8ad
엄지윤 <@DESKTOP-123CLQE>
2020-07-27 17:08:11 +0900

    JDBC

--- jdbc/src/com/bit2020/jdbc/ConnectionTest.java
+++ jdbc/src/com/bit2020/jdbc/ConnectionTest.java
@@ -1,5 +1,55 @@
 package com.bit2020.jdbc;
 
-public class ConnectionTest {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class ConnectionTest {
+	public static void main(String[] args){
+		Connection connection = null;
+		Statement stmt = null;
+		try {
+			// 1. JDBC Driver(MariaDB Driver)
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. 연결하기
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?characterEncoding=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+		
+			// 3. Statement 객체 생성
+			stmt = connection.createStatement();
+			
+			// 4. SQL 실행
+			String firstName = "엄";
+			String lastName = "지윤";
+			String email = "yunn1209@gmail.com";
+			String sql = "insert into emaillist values(null, '" + 
+			firstName + "', '" + lastName + "', '" + email + "')";
+			
+			int count = stmt.executeUpdate(sql);
+			if(count == 1) {
+				System.out.println("성공!");
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("에러" + e);
+
+		} finally {
+			try {
+				if(stmt != null) {
+					stmt.close();
+				}
+				if(connection != null)
+					connection.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
 }
