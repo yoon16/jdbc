fd88c6a69ce53e03825769d7e995b1ade075a0a9
엄지윤 <@DESKTOP-123CLQE>
2020-07-30 22:08:38 +0900

    jdbc

--- jdbc/src/com/bit2020/jdbc/ConnectionTest.java
+++ jdbc/src/com/bit2020/jdbc/ConnectionTest.java
@@ -22,7 +22,7 @@ public class ConnectionTest {
 			System.out.println("드라이버 로딩 실패");
 		} catch (SQLException e) {
 			System.out.println("에러:" + e);
-		} finally {
+		} finally { //
 			try {
 				if(connection != null) {
 					connection.close();
